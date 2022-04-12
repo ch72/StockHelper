77d8bd6a1e4664c1d7b69ebf18628d5d813a384a
Healveon <chong2744sdsu@gmail.com>
2022-04-12 11:24:23 -0700

    Added commands to modify database and view database + minor edits
    Added searchTable, insertRow, showTable, and deleteRow. Also shuffled function parameters and edited error statements to make it easier to locate where the error originiated

--- database.py
+++ database.py
@@ -14,29 +14,29 @@ def mysql_init(hostname, username, passwrd):
   return mydb
 
 # Creates a database on connector (dbinstance)
-def createDatabase(dbname, dbinstance):
+def createDatabase(dbinstance, dbname):
 
   line = dbinstance.cursor()
 
   # Creates a database if it does not exist
   try: line.execute(f"CREATE DATABASE {dbname}")
-  except: print("Database already exists")
+  except: print("createDatabase: Database already exists")
 
 # Creates a table in a database on connector
-def createTable(tablename, dbinstance, columnDict, dbname = None):
+def createTable(dbinstance, tablename, columnsForTable, dbname = None):
 
   line = dbinstance.cursor()
 
-  cols = "CREATE TABLE " + tablename + "("
-  for columns, types in columnDict.items():
-    cols += columns + " " + types + ", "
-  cols = cols[0:len(cols)-2] + ")"
+  command = "CREATE TABLE " + tablename + "("
+  for columns, types in columnsForTable.items():
+    command += columns + " " + types + ", "
+  command = command[0:-2] + ")"
 
   if (dbname != None): line.execute(f"USE {dbname}")
 
   # Creates a table if it does not exist
-  try: line.execute(cols)
-  except: print("Table already exists")
+  try: line.execute(command)
+  except: print("createTable: Table already exists")
 
 # Deletes a database on connector
 def deleteDatabase(dbinstance, dbname):
@@ -44,25 +44,79 @@ def deleteDatabase(dbinstance, dbname):
   line = dbinstance.cursor()
 
   try: line.execute(f"DROP DATABASE {dbname}")
-  except: print("Database for deletion does not exist")
+  except: print("deleteDatabase: Database for deletion does not exist")
 
 # Deletes a table in a database on connector
-def deleteTable(tablename, dbinstance, dbname = None):
+def deleteTable(dbinstance, tablename, dbname = None):
 
   line = dbinstance.cursor()
 
-  if (dbname == None): line.execute(f"USE {dbname}")
+  if (dbname != None): line.execute(f"USE {dbname}")
 
   try: line.execute(f"DROP TABLE {tablename}")
-  except: print("Table for deletion does not exist")
+  except: print("deleteTable: Table for deletion does not exist")
 
-#TODO: Finish by adding columns
-# Adds a row to the specified table
-def insertRow(tablename, dbinstance, columns, dbname = None):
+# Searches table for desired value(s)
+def searchTable(dbinstance, tablename, condition = None, orderBy = None, columnsToShow = None, dbname = None):
 
   line = dbinstance.cursor()
 
-  if (dbname == None): line.execute(f"USE {dbname}")
+  command = "SELECT "
 
-  try: line.execute(f"ALTER TABLE {tablename} ADD ")
-  except: print("Inputted table does not exist")
+  if (columnsToShow != None):
+    for column in columnsToShow:
+      command += str(column) + ", "
+    command = command[0:-2]
+  else: command += "*"
+
+  command += " FROM " + tablename
+
+  # Currently, functional only supports one condition (TODO: Allow for more)
+  if (condition != None): command += " WHERE " + condition 
+
+  if (orderBy != None): command += " ORDER BY " + orderBy
+  
+  if (dbname != None): line.execute(f"USE {dbname}")
+
+  try: 
+    line.execute(command)
+    return (line.fetchall())
+  except: print("searchTable: Error")
+
+# Displays table in its entirity 
+def showTable(dbinstance, tablename, columnsToShow = None, orderBy = None, dbname = None):
+
+  line = dbinstance.cursor()
+  table = searchTable(dbinstance, tablename, None, orderBy, columnsToShow, dbname)
+  for entry in table:
+    print(entry)
+
+# Adds a row to the specified table
+def insertRow(dbinstance, tablename, columnValues, dbname = None):
+
+  line = dbinstance.cursor()
+
+  if (dbname != None): line.execute(f"USE {dbname}")
+
+  command = "INSERT INTO " + tablename + " VALUES("
+  for column in columnValues: 
+    if (isinstance(column, str)): command += '\'' + column + '\'' + ", "
+    else: command += str(column) + ", "
+  command = command[0:-2] + ")"
+
+  try: 
+    line.execute(command)
+    dbinstance.commit()
+  except: print("insertRow: Inputted table does not exist or values are invalid")
+
+# Deletes a row from the specified table
+def deleteRow(dbinstance, tablename, condition = None, dbname = None):
+
+  line = dbinstance.cursor()
+
+  if (dbname != None): line.execute(f"USE {dbname}")
+
+  try: 
+    line.execute(f"DELETE FROM {tablename} WHERE {condition}")
+    dbinstance.commit()
+  except: print ("deleteRow: Inputted table does not exist or condition is invalid")
